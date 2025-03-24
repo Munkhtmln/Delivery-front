ef6967b11659f34062f56c0e06cc930933a2f6c0
Munkhtmln <munkhtemuulen0809@gmail.com>
2025-03-24 10:13:54 +0800

    gg

--- src/app/components/Adddish.tsx
+++ src/app/components/Adddish.tsx
@@ -8,10 +8,54 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export default function AddDish() {
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    // defaultValues: {
+    //   name: "",
+    //   price: "",
+    //   ingredients: "",
+    //   image: "",
+    // },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log("NEMDEG AJILJ EHELLEE");
+    // const imageUrl = await uploadImage(foodImageFile);
+    // if (!imageUrl) return;
+    // const response = await fetch("http://localhost:4000/food", {
+    //   method: "",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({
+    //     categoryName: values.categoryName,
+    //     price: values.price,
+    //     image: imageUrl,
+    //     ingredients: values.ingredients,
+    //     category: "",
+    // //   }),
+    // });
+  };
+
   return (
     <div className="bg-white w-[100%] mt-10 ">
       <Dialog>
@@ -28,7 +72,60 @@ export default function AddDish() {
           </div>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
+          <DialogTitle></DialogTitle>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex gap-4">
+                      <div className="w-[50%]">
+                        <FormLabel>Food name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="name" {...field} />
+                        </FormControl>
+                      </div>
+                      <div className="w-[50%]">
+                        <FormLabel>Food price</FormLabel>
+                        <FormControl>
+                          <Input placeholder="price" {...field} />
+                        </FormControl>
+                      </div>
+                    </div>
+                    <FormLabel>Ingredients</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="ingredients"
+                        className="col-span-3 pb-10 pt-4"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormLabel></FormLabel>
+                    <FormControl>
+                      <Input
+                        type="file"
+                        placeholder="ingredients"
+                        className="h-[100px] flex items-center align-middle justify-center"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />{" "}
+            </form>
+          </Form>
+          <Button type="submit">Add dish</Button>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+{
+  /* <DialogHeader>
             <DialogTitle>Add new dish to...........</DialogTitle>
             <DialogDescription></DialogDescription>
           </DialogHeader>
@@ -76,9 +173,5 @@ export default function AddDish() {
           </div>
           <DialogFooter>
             <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+          </DialogFooter> */
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,39 +1,22 @@
-import { ChevronLeft } from "lucide-react";
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+import FirstPage from "./firstpage";
+import SecondPage from "./secondpage";
 
 export default function Signup() {
+  const [ChangePage, setChangePage] = useState<number>(0);
+  const FormStep = [FirstPage, SecondPage][ChangePage];
+  const [mail, setMail] = useState("");
+
+  const next = () => {
+    setChangePage(ChangePage + 1);
+  };
+
   return (
-    <div className="flex gap-5 m-auto max-w-[2000px] max-h-[2000px] mt-[50px]">
-      <div className="flex flex-col max-w-[416px]  h-[288px] m-auto items-start">
-        <button className="mb-8 border-[1px] rounded-sm">
-          <ChevronLeft />
-        </button>
-        <h1>Create your account </h1>
-        <p className="text-gray-500 ">
-          Sign up to explore your favorite dishes.
-        </p>
-        <input
-          className="border-2 w-[416px] h-[36px] rounded-sm mt-8 pl-4"
-          type="mail"
-          name=""
-          id=""
-          placeholder="Enter your email address"
-        />
-        <Link href={"./signup/password"}>
-          <button className="w-[416px] h-[36px] rounded-sm mt-8 bg-gray-300">
-            Let's Go
-          </button>
-        </Link>
-        <div className="flex mt-8 justify-center w-[416px] gap-2">
-          <p className="text-gray-500">Already have an account?</p>{" "}
-          <Link href={"/login"}>
-            <p className="text-blue-600">Log in</p>
-          </Link>
-        </div>
-      </div>
-      <div className="  m-auto h-[100%]">
-        <img className="w-[856px] h-[904px]" src="./Frame.svg" alt="" />
-      </div>
+    <div>
+      {/* <FormStep next={next} /> */}
+      <FormStep next={next} mail={mail} setMail={setMail} />
     </div>
   );
 }
